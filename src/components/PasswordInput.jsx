4e55d6c5f553ca9cbc88dbd41e85d7756a00660d
Ann Grace Evans <agevans789@gmail.com>
2025-11-11 16:38:48 -0500

    1 test passing

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,8 +1,27 @@
 // Code PasswordInput Component Here
 
 function PasswordInput (){
+    // render input with password type
+    // add change event to the <input>
+    const handleChange = (e) => {
+        console.log("Entering password...");
+        setPassword(e.target.value);
+    };
     return(
-        <></>
+        <div>
+            <label htmlFor="password-field">Enter Password:</label>
+            <input
+            id="password-field"
+            type="password"
+            value={password}
+            onChange={handleChange}
+            placeholder="Password"
+            style={{marginLeft: '10px', padding: '5px'}}
+            />
+            <p>
+                Current Input Value: <code>{password}</code>
+            </p>
+        </div>
     )
 }
 
--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,8 +1,17 @@
 // Code SubmitButton Component Here
 
 function SubmitButton (){
+    // add mouse enter and leave event 
+    const handleEnter = () => {
+        console.log("Mouse entering...")
+    };
+    const handleLeave = () => {
+        console.log("Mouse exiting...")
+    };
     return(
-        <></>
+        <>
+            <button onMouseEnter="" onMouseLeave="">Submit Password</button>
+        </>
     )
 }
 
